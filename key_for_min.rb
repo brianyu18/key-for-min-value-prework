916696e5ebbf7079e25ba36c899cb770f284cc2d
brianyu18 <brianyu1990@gmail.com>
2018-09-27 16:51:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,10 @@ def key_for_min_value(name_hash)
   result = 0
   answer = ''
   name_hash.each do |key, value|
-    if value > result
-      value = key
+    if value == {}
+      return nil
+    elsif value > result
+      answer = key
     end
   end
 answer.to_s
